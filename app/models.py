30db83354d2b2c0cd4f9882681d3b1c208d7b0ad
Alfonso Sargiotto <a.sargiotto@itecriocuarto.org.ar>
2022-06-27 20:25:28 -0300

    refactor models

--- app/models.py
+++ app/models.py
@@ -4,28 +4,35 @@ DataBase models declaration
 
 from app import db
 
+
 class Movie(db.Model):
     id = db.Column(db.Integer(), primary_key=True, unique=True, nullable=False)
     title = db.Column(db.String(150), nullable=False)
-    director_id = db.Column(db.Integer(), db.ForeignKey("director.id"), default=1, nullable=False)
-    category_id = db.Column(db.Integer(), db.ForeignKey("category.id"), default=1, nullable=False)
+    director_id = db.Column(
+        db.Integer(), db.ForeignKey("director.id"), default=1, nullable=False
+    )
+    category_id = db.Column(
+        db.Integer(), db.ForeignKey("category.id"), default=1, nullable=False
+    )
     show_type = db.Column(db.String(20))
     cast = db.Column(db.String(1000))
     country = db.Column(db.String(150))
     release_year = db.Column(db.Integer())
     rating = db.Column(db.String(50))
+    score = db.Column(db.Float(0, 1))
     duration = db.Column(db.String(50))
-    description = db.Column(db.String(255))
+    description = db.Column(db.String(1000))
+    director = db.relationship("Director", backref="movie", lazy=True)
+    category = db.relationship("Category", backref="movie", lazy=True)
+
 
 class Director(db.Model):
     id = db.Column(db.Integer(), primary_key=True, unique=True, nullable=False)
     name = db.Column(db.String(255), nullable=False)
     birth_date = db.Column(db.Date())
     is_alive = db.Column(db.Boolean(), default=True)
-    
+
+
 class Category(db.Model):
     id = db.Column(db.Integer(), primary_key=True, unique=True, nullable=False)
     name = db.Column(db.String(30), nullable=True)
-
-
-
