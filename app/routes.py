76d1b1eca316786d8414d3d743203d6f25111700
Alfonso Sargiotto <a.sargiotto@itecriocuarto.org.ar>
2022-07-19 22:20:13 -0300

    add edit movie feature

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,17 @@
 from flask import (
     render_template,
+    url_for,
     abort,
     flash,
+    redirect,
     request,
 )
 from sqlalchemy import or_
 
 from app import app, db
+from app.forms import MovieForm
 from app.models import Director, Movie, Category
+from app.helpers.models_formatter import get_tuples
 
 
 @app.route("/")
@@ -66,3 +70,54 @@ def top_ten():
     top_ten_movies = Movie.query.order_by(Movie.score.desc()).limit(10)
 
     return render_template("top_ten_movies.html", movies=top_ten_movies)
+
+
+@app.route("/new_movie", methods=["GET", "POST"])
+def new_movie():
+    categories_list = get_tuples(Category.query.order_by(Category.name.asc()).all())
+    directors_list = get_tuples(Director.query.order_by(Director.name.asc()).all())
+    form = MovieForm()
+    form.category_id.choices = categories_list
+    form.director_id.choices = directors_list
+    if form.validate_on_submit():
+        movie = Movie(
+            title=form.title.data,
+            director_id=form.director_id.data,
+            category_id=form.category_id.data,
+            rating=form.rating.data,
+            show_type=form.show_type.data,
+            cast=form.cast.data,
+            country=form.country.data,
+            release_year=form.release_year.data,
+            score=form.score.data,
+            duration=form.duration.data,
+            description=form.description.data,
+        )
+
+        db.session.add(movie)
+        db.session.commit()
+        flash("Movie succesfully created!", "success")
+        return redirect(url_for("movie"))
+
+    template_title = "New Movie"
+    return render_template("new_movie.html", form=form, template_title=template_title)
+
+
+@app.route("/movie/edit/<movie_id>", methods=["GET", "POST"])
+def edit_movie(movie_id):
+    categories_list = get_tuples(Category.query.order_by(Category.name.asc()).all())
+    directors_list = get_tuples(Director.query.order_by(Director.name.asc()).all())
+    movie = Movie.query.filter_by(id=movie_id).first()
+    form = MovieForm(obj=movie)
+    form.category_id.choices = categories_list
+    form.director_id.choices = directors_list
+
+    if request.method == "POST" and form.validate_on_submit():
+        form.populate_obj(movie)
+        db.session.commit()
+
+        flash("Movie succesfully modified!", "info")
+        return redirect(url_for("movie"))
+
+    template_title = "Update Movie"
+    return render_template("new_movie.html", form=form, template_title=template_title)
